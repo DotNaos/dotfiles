7661f9c04c479fe26d900a51b45fbe4e9e97f3d1
DotNaos <schuetzoliver00@gmail.com>
2025-08-06 17:33:51 +0200

    Add Vim Plugins

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,1 +1,20 @@
-return {}
+return {
+  {
+    "tpope/vim-fugitive",
+    cmd = { "Git", "G", "Gdiffsplit", "Gvdiffsplit", "Gedit", "Gsplit", "Gvsplit", "Gtabedit", "Gpedit" },
+    keys = {
+      { "<leader>gs", "<cmd>Git<cr>", desc = "Git status" },
+      { "<leader>gb", "<cmd>Git blame<cr>", desc = "Git blame" },
+      { "<leader>gd", "<cmd>Gdiffsplit<cr>", desc = "Git diff split" },
+    },
+  },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
+}
